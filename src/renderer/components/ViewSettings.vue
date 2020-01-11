daad208a655dd9786d5760b6924b7891df3339d1
Tobias Schultheiß <tobi@strasserhof.com>
2020-01-11 16:23:47 +0100

    remade setting view

--- src/renderer/components/ViewSettings.vue
+++ src/renderer/components/ViewSettings.vue
@@ -2,30 +2,37 @@
 	<div class="view-settings">
 		<div class="view-settings-header">
 			<span>Settings</span>
-			<ui-icon-button icon="close" type="secondary" color="red" @click="$router.go(-1)" />
 		</div>
 		<div class="view-settings-content">
 			<ui-setting label="Username">
-				username: {{ username }}
-				<ui-textbox v-model="username" placeholder="username..." />
+				<ui-textbox :value="getSettingValue('username')" placeholder="username..." @input="eventInputUsername" />
+			</ui-setting>
+
+			<ui-setting label="Status">
+				<ui-textbox :value="getSettingValue('status')" placeholder="status..." @input="eventInputStatus" />
 			</ui-setting>
 
 			<ui-setting label="Public Key">
 				<span>
-					{{ getPublicKey }}
+					{{ getSettingValue('publicKey') }}
 				</span>
 			</ui-setting>
-
 			<ui-setting label="Private Key">
 				<span>
-					{{ getPrivateKey }}
+					{{ getSettingValue('privateKey') }}
 				</span>
 			</ui-setting>
-			<ui-button color="primary" type="secondary" @click="eventRegenerateKeyPair">Regenerate Key Pair</ui-button>
+			<ui-setting>
+				<ui-button color="primary" type="secondary" @click="eventRegenerateKeyPair">Regenerate Key Pair</ui-button>
+			</ui-setting>
+			
 		</div>
 		<div class="view-settings-footer">
-			<ui-button color="green" type="primary" @click="eventSaveSettings">Save</ui-button>
-			<ui-button color="red" type="secondary" @click="$router.go(-1)">cancel</ui-button>
+			<ui-button color="green" type="primary" :disabled="saveButtonDisabled" @click="eventSaveSettings">Save</ui-button>
+			<ui-button color="red" type="secondary" @click="eventClickCancel">cancel</ui-button>
+			<ui-confirm ref="discardChangesModal" title="Discard changes" @confirm="$router.go(-1)">
+				Do you want to discard all changes?
+			</ui-confirm>
 		</div>
 	</div>
 </template>
@@ -33,6 +40,7 @@
 import UiSetting from './ui/UiSetting'
 import network from '../../main/network'
 import { mapGetters } from 'vuex'
+import generateKey from '../../main/helper'
 
 export default {
 	
@@ -44,29 +52,64 @@ export default {
 
 	data () {
 		return {
-			username: ''
+			initialSettings: {},
+			settings: {},
 		}
 	},
 
 	mounted(){
-		this.username = this.getUsername
+		this.initialSettings = this.getSettings
 	},
 
 	computed: {
 		...mapGetters([
-			'getUsername',
-			'getPublicKey',
-			'getPrivateKey',
-		])
+			'getSettings',
+		]),
+		settingss() {
+			return this.settings
+		},
+		saveButtonDisabled() {
+			return Object.keys(this.settings).length === 0
+		}
 	},
 
 	methods: {
+		getSettingValue(name) {
+			if(Object.keys(this.settings).includes(name)){
+				return this.settings[name]
+			}
+			return this.initialSettings[name]
+		},
+		setSettingValue(name, value) {
+			if(value === this.initialSettings[name]){
+				this.$delete(this.settings, name)
+			}
+			else {
+				this.$set(this.settings, name, value)
+			}
+		},
+		eventInputUsername(value) {
+			this.setSettingValue('username', value)
+		},
+		eventInputStatus(value) {
+			this.setSettingValue('status', value)
+		},
 		eventRegenerateKeyPair() {
-
+			this.setSettingValue('publicKey', generateKey())
+			this.setSettingValue('privateKey', generateKey())
 		},
 		eventSaveSettings(){
-			this.$store.dispatch('username', this.username)
-			network.sendUserUpdate()
+			this.$store.dispatch('settings', this.settings)
+			network.sendUserUpdate(this.getPublicKey, this.username, 'das ist ein status')
+			this.$router.go(-1)
+		},
+		eventClickCancel(){
+			if(!this.saveButtonDisabled){
+				this.$refs.discardChangesModal.open()
+			}
+			else{
+				this.$router.go(-1)
+			}
 		}
 	}
 }
@@ -83,6 +126,7 @@ export default {
 	display: flex;
 	flex-direction: column;
 	background-color: @primary;
+	border-top-left-radius: 16px;
 	&-header {
 		display: flex;
 		align-items: center;
