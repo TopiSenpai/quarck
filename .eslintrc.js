96482666c2012cf195aed7aec43c68357da9b20a
thorio <thorio.git@gmail.com>
2020-01-11 17:04:54 +0100

    Configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es6": true,
+		"node": true,
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:vue/essential",
+	],
+	"globals": {
+		"Atomics": "readonly",
+		"SharedArrayBuffer": "readonly",
+	},
+	"parserOptions": {
+		"ecmaVersion": 2018,
+		"sourceType": "module",
+	},
+	"plugins": [
+		"vue",
+	],
+	"rules": {
+		"indent": ["warn", "tab"],
+		"quotes": ["warn", "double"],
+		"semi": ["warn", "always"],
+		"require-atomic-updates": "warn",
+		"no-unused-vars": "warn",
+		"no-useless-escape": "warn",
+		"no-await-in-loop": "warn",
+		"dot-location": ["warn", "property"],
+		"semi": "warn",
+		"comma-dangle": ["warn", {
+			"arrays": "always-multiline",
+			"objects": "always-multiline",
+		}],
+		"no-constant-condition": ["warn", {
+			"checkLoops": false,
+		}],
+	}
+};
