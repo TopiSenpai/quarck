e8ccdb00416ed4eab1ba96864e17cd7e45a31498
ToπSenpai <tobi@strasserhof.com>
2020-01-12 05:38:47 +0100

    removed tcp port from packet

--- src/main/packets/DiscoverClientsPacket.js
+++ src/main/packets/DiscoverClientsPacket.js
@@ -3,8 +3,8 @@ import PacketTypes from "./PacketTypes";
 
 export default class DiscoverClientsPacket extends Packet {
 
-	constructor(publicKey, username, imageUrl, status, tcpPort) {
-		super(PacketTypes.DiscoverClients, { key: publicKey, username: username, image: imageUrl, status: status, tcp_port: tcpPort });
+	constructor(publicKey, username, imageUrl, status) {
+		super(PacketTypes.DiscoverClients, { key: publicKey, username: username, image: imageUrl, status: status });
 	}
 
 }
